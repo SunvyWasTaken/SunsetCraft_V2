261b9e8a784be7dee38aee1259b5b583532e8890
DevOfSomewhere <68583362+SunvyWasTaken@users.noreply.github.com>
2026-06-16 14:48:43 +0200

    HaloGen less memory

--- Sources/WorldGen/HaloGen.cpp
+++ Sources/WorldGen/HaloGen.cpp
@@ -7,6 +7,21 @@
 #include "FastNoiseSIMD.h"
 #include "../Chunk.h"
 
+namespace
+{
+    template <typename T>
+    requires std::is_arithmetic_v<T>
+    [[nodiscard]]float Normalize(T min, T max, T value)
+    {
+        return static_cast<float>(value - min) / static_cast<float>(max - min);
+    }
+
+    float Lerp(float a, float b, float t)
+    {
+        return a + t * (b - a);
+    }
+}
+
 struct HaloGen::Impl
 {
     std::unique_ptr<FastNoiseSIMD> noise = nullptr;
@@ -33,14 +48,20 @@ void HaloGen::operator()(Chunk &chunk, ChunkData &data)
 {
     SS_PROFILE_FUNCTION();
     (*impl)()->SetSeed(data.seed);
-    float* NoiseSet = FastNoiseSIMD::GetEmptySet(SIZE_X, SIZE_Y + SIZE_Y, SIZE_Z);
+    float* NoiseSet = FastNoiseSIMD::GetEmptySet(SIZE_Y + SIZE_Y);
 
-    (*impl)()->FillNoiseSet(NoiseSet, chunk.m_Position.x * SIZE_X, 0, SIZE_Z, SIZE_X, SIZE_Y + SIZE_Y, SIZE_Z);
-    for (size_t i = 0; i < chunk.m_Blocks.size(); ++i)
+    for (int x = 0; x < SIZE_X; ++x)
     {
-        if (std::abs(NoiseSet[i]) < 0.01f)
+        for (int z = 0; z < SIZE_Z; ++z)
         {
-            chunk.m_Blocks[i] = BlockRegistry::AIR;
+            (*impl)()->FillNoiseSet(NoiseSet, chunk.m_Position.x * SIZE_X + x, -SIZE_Y, chunk.m_Position.y * SIZE_Z + z, 1, SIZE_Y + SIZE_Y, 1);
+            for (int y = -SIZE_Y; y < SIZE_Y; ++y)
+            {
+                if (std::abs(NoiseSet[y + SIZE_Y]) < Lerp(0, 0.03f, Normalize(-SIZE_Y, SIZE_Y, y)))
+                {
+                    chunk.m_Blocks[x + z * SIZE_X + (y + SIZE_Y) * SIZE_X * SIZE_Z] = BlockRegistry::AIR;
+                }
+            }
         }
     }
     FastNoiseSIMD::FreeNoiseSet(NoiseSet);
