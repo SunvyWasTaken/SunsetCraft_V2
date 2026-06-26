bec93c1661690c0815a68cde6fd43bc1c27b3bb3
DevOfSomewhere <68583362+SunvyWasTaken@users.noreply.github.com>
2026-06-26 22:36:55 +0200

    Test logo

--- Sources/MainMenu.cpp
+++ Sources/MainMenu.cpp
@@ -10,14 +10,39 @@
 #include "Core/Application.h"
 #include "Core/ApplicationSetting.h"
 #include "Network/NetworkService.h"
+#include "Render/Texture.h"
 #include "Widget/UIImage.h"
+
+class LogoImage final : public Sunset::UIImage
+{
+public:
+    explicit LogoImage()
+        : UIImage()
+    {
+        SetDesiredSize({220, 56});
+        m_Texture = std::make_shared<Sunset::Texture>();
+        m_Texture->LoadImage(RESOURCES "Logo/Logo.png");
+    }
+
+    void Arrange(const Sunset::Rectangle& parentRect) override
+    {
+        const glm::ivec2 size = GetDesiredSize();
+
+        m_Bounds.position = {
+            parentRect.position.x + 40,
+            parentRect.position.y + 40
+        };
+
+        m_Bounds.size = size;
+    }
+};
 
 MainMenu::MainMenu()
 {
-    std::shared_ptr<Sunset::UIImage> image = std::make_shared<Sunset::UIImage>();
-    image->LoadImage(RESOURCES "Logo/Logo.png");
+    std::shared_ptr<LogoImage> image = std::make_shared<LogoImage>();
+    // image->LoadImage(RESOURCES "Logo/Logo.png");
     image->m_Bounds.position = Sunset::Application::GetSetting().WindowSize / 2;
-    image->SetDesiredSize({2172, 724});
+    image->SetDesiredSize({250, 50});
     AddToViewport(image);
 }
 
